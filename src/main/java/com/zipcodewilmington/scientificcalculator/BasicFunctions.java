8a6a54b39856cf86c436e8b253970aa5ae9decaf
Dee <dee@zipcodes-MacBook-Pro-2.local>
2021-06-27 20:04:17 -0400

    run the tests Manny

--- src/main/java/com/zipcodewilmington/scientificcalculator/BasicFunctions.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/BasicFunctions.java
@@ -10,11 +10,20 @@ public class BasicFunctions {
    public static Double multiplyIt(double input1, double input2){
         return input1 * input2;
     }
-    public static Double divideIt(double input1, double input2){
-        return input1/input2;
+    public static Double divideIt(double input1, double input2) {
+        return input1 / input2;
     }
-    public static Double squareIt(double input1, double input2) {
-        return Math.pow(input1,input2);
+        /*double answer;
+        //if (input2 == 0) {
+            Console.println("Error. Don't do that");
+        } else {
+            answer = input1/input2;
+            return answer;
+        }
+    */
+
+    public static Double squareIt(double input) {
+        return Math.pow(input,2);
     }
     public static Double squareRootIt(double input){
         return Math.sqrt(input);
@@ -24,6 +33,9 @@ public class BasicFunctions {
     }
     public static Double changeSigns(double input){
         return input * (-1) ;
+    }
+    public static Double powerExponent(double input1, double input2){
+        return Math.pow(input1,input2);
     }
     public static Boolean isError(){
         //if input is Not a Number return Error
