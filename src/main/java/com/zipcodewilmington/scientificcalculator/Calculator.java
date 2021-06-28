0fea0dba52d3922e2ca6ee0d5f882d5155585778
Manny <manny@ZCW-JLAT.home>
2021-06-27 20:05:28 -0400

    scientificmenu halfway done

--- src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Calculator.java
@@ -11,11 +11,15 @@ public class Calculator {
         Console.println("2: Scientific");
         input = Console.getIntegerInput("Enter number for menu options");
 
-        if(input == 2){
-            scientificOptionsMenu();
-        } else {
+        if(input == 1){
             basicOptionsMenu();
+        } if (input == 2) {
+            scientificOptionsMenu();
+        } else if (input != 1 && input != 2) {
+            Console.getIntegerInput("Please try again...Enter a number from the menu options \n>");
+            topMenu();
         }
+
     }
 
     public static void basicOptionsMenu(){
@@ -63,21 +67,33 @@ public class Calculator {
         int input;
 
         Console.println("1: Trigonometry");
-        Console.println("2: Logarithms");
-        Console.println("3: Factorials");
+        Console.println("2: Factorials");
+        Console.println("3: Logarithms");
         Console.println("4: Return to Top Menu");
 
-        input = Console.getIntegerInput("Enter number for menu option");
+        input = Console.getIntegerInput("Enter number from the menu option");
 
-        // Double inputA = Console.getDoubleInput("First number");
+        //Double inputA = Console.getDoubleInput("First number");
+        //Integer inputB = Console.getIntegerInput("First number");
 
         switch (input) {
             case 1:
                 trigOptionsMenu();
                 break;
             case 2:
-
-
+                Integer inputB = Console.getIntegerInput("First number");
+                Integer factorialValue = ScientificFunctions.getFactorial(inputB);
+                Console.println(factorialValue.toString());
+                state = factorialValue;
+                break;
+            case 3:
+                logOptionsMenu();
+                break;
+            case 4:
+                topMenu();
+                break;
+            default:
+                break;
         }
     }
 
@@ -90,9 +106,9 @@ public class Calculator {
         Console.println("4: Inverse Sine");
         Console.println("5: Inverse Cosine");
         Console.println("6: Inverse Tangent");
-        Console.println("7: Return to Top Menu");
+        Console.println("7: Return to Trigonometry Menu");
 
-        trigSelector = Console.getIntegerInput("Enter number for Trigonometry menu");
+        trigSelector = Console.getIntegerInput("Enter a number from the Trigonometry menu");
 
 
         Double trigNumber = Console.getDoubleInput("First number");
@@ -128,7 +144,57 @@ public class Calculator {
                 Console.println(inverseTangentValue.toString());
                 state = inverseTangentValue;
                 break;
+            case 7:
+                trigOptionsMenu();
+
+            default:
+                topMenu();
         }
     }
+
+    private static void logOptionsMenu() {
+        int logSelector;
+
+        Console.println("1: Log");
+        Console.println("2: Natural Log");
+        Console.println("3: Inverse Log");
+        Console.println("4: Inverse Natural Log");
+        Console.println("5: Return to Trigonometry Menu");
+
+        logSelector = Console.getIntegerInput("Enter a number from the Trigonometry menu");
+
+
+        Double logNumber = Console.getDoubleInput("First number");
+
+        switch (logSelector) {
+            case 1:
+                Double logValue = ScientificFunctions.getSine(logNumber);
+                Console.println(logValue.toString());
+                state = logValue;
+                break;
+            case 2:
+                Double naturalLogValue = ScientificFunctions.getCosine(logNumber);
+                Console.println(naturalLogValue.toString());
+                state = naturalLogValue;
+                break;
+            case 3:
+                Double inverseLogValue = ScientificFunctions.getCosine(logNumber);
+                Console.println(inverseLogValue.toString());
+                state = inverseLogValue;
+                break;
+            case 4:
+                Double inverseNaturalLogValue = ScientificFunctions.getSine(logNumber);
+                Console.println(inverseNaturalLogValue.toString());
+                state = inverseNaturalLogValue;
+                break;
+            case 5:
+                logOptionsMenu();
+
+            default:
+                topMenu();
+
+        }
+    }
+
 
 }
